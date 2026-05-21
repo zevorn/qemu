fd6da94b6dffcba0787ff550faeb106cee80c033
Chao Liu <chao.liu.zevorn@gmail.com>
2026-05-21 10:44:16 +0800

    hw/misc: add K230 IPCM interrupt state
    The hardlock block also exposes IPCM interrupt registers used by the SDK mailbox path.  Add the interrupt enable, set, clear and status behavior and expose four IRQ lines so the board can route DSP-to-CPU events.
    
    Signed-off-by: Chao Liu <chao.liu.zevorn@gmail.com>

--- include/hw/misc/k230_hardlock.h
+++ include/hw/misc/k230_hardlock.h
@@ -8,6 +8,7 @@
 #define HW_MISC_K230_HARDLOCK_H
 
 #include "hw/core/sysbus.h"
+#include "hw/core/irq.h"
 #include "qom/object.h"
 
 #define TYPE_K230_HARDLOCK "riscv.k230.hardlock"
@@ -15,6 +16,7 @@ OBJECT_DECLARE_SIMPLE_TYPE(K230HardlockState, K230_HARDLOCK)
 
 #define K230_HARDLOCK_SIZE 0x1000
 #define K230_HARDLOCK_COUNT 128
+#define K230_HARDLOCK_IPCM_IRQ_COUNT 4
 
 struct K230HardlockState {
     /*< private >*/
@@ -22,6 +24,7 @@ struct K230HardlockState {
 
     /*< public >*/
     MemoryRegion mmio;
+    qemu_irq irqs[K230_HARDLOCK_IPCM_IRQ_COUNT];
     uint8_t regs[K230_HARDLOCK_SIZE];
     bool locks[K230_HARDLOCK_COUNT];
 };
