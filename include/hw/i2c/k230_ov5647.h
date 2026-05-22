94eea827a0c47d030e06eb611b78496766b14621
Chao Liu <chao.liu.zevorn@gmail.com>
2026-05-22 09:38:59 +0800

    hw/i2c: add K230 OV5647 sensor stub
    Add a small OV5647 I2C model that exposes the chip ID and writable sensor register space expected by the K230 camera initialization path.
    
    Signed-off-by: Chao Liu <chao.liu.zevorn@gmail.com>

---
+++ include/hw/i2c/k230_ov5647.h
@@ -0,0 +1,14 @@
+/*
+ * K230 OV5647 camera sensor stub
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_I2C_K230_OV5647_H
+#define HW_I2C_K230_OV5647_H
+
+#include "hw/i2c/i2c.h"
+
+#define TYPE_K230_OV5647 "k230.ov5647"
+
+#endif /* HW_I2C_K230_OV5647_H */
