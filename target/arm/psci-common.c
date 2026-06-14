289216ec46718d12cdb66cd3b04c3c1ed6c61a62
Chao Liu <chao.liu@processmission.com>
2026-06-14 20:32:58 +0800

    target/arm: add board SMC hook support
    Signed-off-by: Chao Liu <chao.liu@processmission.com>

---
+++ target/arm/psci-common.c
@@ -0,0 +1,28 @@
+/*
+ * Common ARM PSCI helpers.
+ *
+ * Copyright (c) 2026 Chao Liu
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+#include "qemu/osdep.h"
+#include "cpu.h"
+#include "internals.h"
+
+/*
+ * Optional board-registered SMC handler. SMC exception paths call this before
+ * generic PSCI handling or architectural SMC behavior. Default NULL.
+ */
+static ArmPsciSmcHandler arm_psci_smc_handler;
+
+void arm_register_psci_smc_handler(ArmPsciSmcHandler handler)
+{
+    arm_psci_smc_handler = handler;
+}
+
+bool arm_handle_psci_smc_handler(ARMCPU *cpu)
+{
+    return arm_psci_smc_handler && arm_psci_smc_handler(cpu);
+}
