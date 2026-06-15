84aea58039dc718ce920826fd8975dab940ee039
Chao Liu <chao.liu@processmission.com>
2026-06-16 03:29:34 +0800

    hw/misc: add K230 IPCM interrupt state
    The hardlock block also exposes IPCM interrupt registers used by the SDK mailbox path.  Add the interrupt enable, set, clear and status behavior and expose four IRQ lines so the board can route DSP-to-CPU events.
    
    Signed-off-by: Chao Liu <chao.liu@processmission.com>

--- hw/misc/k230_hardlock.c
+++ hw/misc/k230_hardlock.c
@@ -7,11 +7,35 @@
  */
 
 #include "qemu/osdep.h"
+#include "qemu/bitops.h"
 #include "qemu/module.h"
 #include "migration/vmstate.h"
 #include "hw/misc/k230_hardlock.h"
 
+#define K230_IPCM_CPU2DSP_INT_EN0       0x0000
+#define K230_IPCM_CPU2DSP_INT_SET0      0x0004
+#define K230_IPCM_CPU2DSP_INT_CLEAR0    0x0008
+#define K230_IPCM_CPU2DSP_INT_STATUS0   0x000c
+#define K230_IPCM_CPU2DSP_INT_ERR0      0x0010
+#define K230_IPCM_DSP2CPU_INT_EN0       0x0014
+#define K230_IPCM_DSP2CPU_INT_SET0      0x0018
+#define K230_IPCM_DSP2CPU_INT_CLEAR0    0x001c
+#define K230_IPCM_DSP2CPU_INT_STATUS0   0x0020
+#define K230_IPCM_DSP2CPU_INT_ERR0      0x0024
+
+#define K230_IPCM_INT_ENABLE            BIT(0)
 #define K230_HARDLOCK_BASE 0xa0
+
+static uint32_t k230_hardlock_readl(K230HardlockState *s, hwaddr addr)
+{
+    return ldl_le_p(s->regs + addr);
+}
+
+static void k230_hardlock_writel(K230HardlockState *s, hwaddr addr,
+                                 uint32_t val)
+{
+    stl_le_p(s->regs + addr, val);
+}
 
 static uint64_t k230_hardlock_read_bytes(uint8_t *regs, hwaddr addr,
                                          unsigned int size)
@@ -51,6 +75,85 @@ static bool k230_hardlock_index(hwaddr addr, unsigned int size,
     return true;
 }
 
+static uint32_t k230_ipcm_channel_mask(uint64_t val)
+{
+    if (val < 32) {
+        return BIT(val);
+    }
+
+    return val;
+}
+
+static void k230_ipcm_update_dsp2cpu_irq(K230HardlockState *s)
+{
+    uint32_t enable = k230_hardlock_readl(s, K230_IPCM_DSP2CPU_INT_EN0);
+    uint32_t status = k230_hardlock_readl(s, K230_IPCM_DSP2CPU_INT_STATUS0);
+    bool irq_enable = enable & K230_IPCM_INT_ENABLE;
+
+    for (int i = 0; i < K230_HARDLOCK_IPCM_IRQ_COUNT; i++) {
+        qemu_set_irq(s->irqs[i], irq_enable && (status & BIT(i)));
+    }
+}
+
+static bool k230_ipcm_write(K230HardlockState *s, hwaddr addr, uint64_t val,
+                            unsigned int size)
+{
+    uint32_t mask;
+    uint32_t status;
+
+    if (size != 4) {
+        return false;
+    }
+
+    switch (addr) {
+    case K230_IPCM_CPU2DSP_INT_EN0:
+    case K230_IPCM_CPU2DSP_INT_ERR0:
+    case K230_IPCM_DSP2CPU_INT_ERR0:
+        k230_hardlock_writel(s, addr, val);
+        return true;
+
+    case K230_IPCM_DSP2CPU_INT_EN0:
+        k230_hardlock_writel(s, addr, val);
+        k230_ipcm_update_dsp2cpu_irq(s);
+        return true;
+
+    case K230_IPCM_CPU2DSP_INT_SET0:
+        mask = k230_ipcm_channel_mask(val);
+        status = k230_hardlock_readl(s, K230_IPCM_CPU2DSP_INT_STATUS0);
+        k230_hardlock_writel(s, K230_IPCM_CPU2DSP_INT_SET0, val);
+        k230_hardlock_writel(s, K230_IPCM_CPU2DSP_INT_STATUS0, status | mask);
+        return true;
+
+    case K230_IPCM_CPU2DSP_INT_CLEAR0:
+        mask = k230_ipcm_channel_mask(val);
+        status = k230_hardlock_readl(s, K230_IPCM_CPU2DSP_INT_STATUS0);
+        k230_hardlock_writel(s, K230_IPCM_CPU2DSP_INT_CLEAR0, val);
+        k230_hardlock_writel(s, K230_IPCM_CPU2DSP_INT_STATUS0,
+                             status & ~mask);
+        return true;
+
+    case K230_IPCM_DSP2CPU_INT_SET0:
+        mask = k230_ipcm_channel_mask(val);
+        status = k230_hardlock_readl(s, K230_IPCM_DSP2CPU_INT_STATUS0);
+        k230_hardlock_writel(s, K230_IPCM_DSP2CPU_INT_SET0, val);
+        k230_hardlock_writel(s, K230_IPCM_DSP2CPU_INT_STATUS0, status | mask);
+        k230_ipcm_update_dsp2cpu_irq(s);
+        return true;
+
+    case K230_IPCM_DSP2CPU_INT_CLEAR0:
+        mask = k230_ipcm_channel_mask(val);
+        status = k230_hardlock_readl(s, K230_IPCM_DSP2CPU_INT_STATUS0);
+        k230_hardlock_writel(s, K230_IPCM_DSP2CPU_INT_CLEAR0, val);
+        k230_hardlock_writel(s, K230_IPCM_DSP2CPU_INT_STATUS0,
+                             status & ~mask);
+        k230_ipcm_update_dsp2cpu_irq(s);
+        return true;
+
+    default:
+        return false;
+    }
+}
+
 static uint64_t k230_hardlock_read(void *opaque, hwaddr addr,
                                    unsigned int size)
 {
@@ -74,6 +177,10 @@ static void k230_hardlock_write(void *opaque, hwaddr addr, uint64_t val,
 {
     K230HardlockState *s = K230_HARDLOCK(opaque);
     unsigned int index;
+
+    if (k230_ipcm_write(s, addr, val, size)) {
+        return;
+    }
 
     k230_hardlock_write_bytes(s->regs, addr, val, size);
 
@@ -123,6 +230,9 @@ static void k230_hardlock_realize(DeviceState *dev, Error **errp)
     memory_region_init_io(&s->mmio, OBJECT(dev), &k230_hardlock_ops, s,
                           TYPE_K230_HARDLOCK, K230_HARDLOCK_SIZE);
     sysbus_init_mmio(SYS_BUS_DEVICE(dev), &s->mmio);
+    for (int i = 0; i < K230_HARDLOCK_IPCM_IRQ_COUNT; i++) {
+        sysbus_init_irq(SYS_BUS_DEVICE(dev), &s->irqs[i]);
+    }
 }
 
 static void k230_hardlock_class_init(ObjectClass *oc, const void *data)
