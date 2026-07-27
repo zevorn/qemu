ceff4e42fdc19032b9c066ce3c80cb89462338f7
Chao Liu <chao.liu.zevorn@gmail.com>
2026-07-28 06:17:22 +0800

    tests/qtest: cover STM32G474 FDCAN reset
    Signed-off-by: Chao Liu chao.liu.zevorn@gmail.com

--- tests/qtest/stm32g474-rcc-test.c
+++ tests/qtest/stm32g474-rcc-test.c
@@ -96,6 +96,7 @@
 
 #define APB1RSTR1_USART2RST (1U << 17)
 #define APB1RSTR1_UART4RST  (1U << 19)
+#define APB1RSTR1_FDCANRST  (1U << 25)
 #define APB1RSTR1_PWRRST    (1U << 28)
 #define APB2RSTR_SYSCFGRST  (1U << 0)
 #define APB2RSTR_USART1RST  (1U << 14)
@@ -147,6 +148,7 @@ enum {
     RCC_RESET_GPIOF,
     RCC_RESET_GPIOG,
     RCC_RESET_SYSCFG,
+    RCC_RESET_FDCAN,
     RCC_RESET_COUNT,
 };
 
@@ -163,7 +165,8 @@ G_STATIC_ASSERT(RCC_RESET_GPIOE == 9);
 G_STATIC_ASSERT(RCC_RESET_GPIOF == 10);
 G_STATIC_ASSERT(RCC_RESET_GPIOG == 11);
 G_STATIC_ASSERT(RCC_RESET_SYSCFG == 12);
-G_STATIC_ASSERT(RCC_RESET_COUNT == 13);
+G_STATIC_ASSERT(RCC_RESET_FDCAN == 13);
+G_STATIC_ASSERT(RCC_RESET_COUNT == 14);
 
 typedef struct RccRegister {
     const char *name;
@@ -257,6 +260,7 @@ static const RccResetOutput reset_outputs[] = {
     { "GPIOF",  RCC_AHB2RSTR,  AHB2RSTR_GPIOFRST,   RCC_RESET_GPIOF },
     { "GPIOG",  RCC_AHB2RSTR,  AHB2RSTR_GPIOGRST,   RCC_RESET_GPIOG },
     { "SYSCFG", RCC_APB2RSTR,  APB2RSTR_SYSCFGRST,  RCC_RESET_SYSCFG },
+    { "FDCAN",  RCC_APB1RSTR1, APB1RSTR1_FDCANRST,  RCC_RESET_FDCAN },
 };
 
 G_STATIC_ASSERT(ARRAY_SIZE(reset_outputs) == RCC_RESET_COUNT);
@@ -845,6 +849,8 @@ static void test_rcc_gates_muxes_resets(void)
         g_test_message("%s reset output", reset_outputs[selected].name);
         rcc_writel(qts, reset_outputs[selected].offset,
                    reset_outputs[selected].bit);
+        g_assert_cmphex(rcc_readl(qts, reset_outputs[selected].offset), ==,
+                        reset_outputs[selected].bit);
         for (unsigned int i = 0; i < RCC_RESET_COUNT; i++) {
             g_assert_cmpint(qtest_get_irq(qts, i), ==,
                             i == reset_outputs[selected].index);
@@ -890,7 +896,7 @@ static void test_rcc_gates_muxes_resets(void)
                APB2RSTR_SYSCFGRST | APB2RSTR_USART1RST);
     rcc_writel(qts, RCC_APB1RSTR1,
                APB1RSTR1_USART2RST | APB1RSTR1_UART4RST |
-               APB1RSTR1_PWRRST);
+               APB1RSTR1_FDCANRST | APB1RSTR1_PWRRST);
     for (unsigned int i = 0; i < RCC_RESET_COUNT; i++) {
         g_assert_true(qtest_get_irq(qts, i));
     }
