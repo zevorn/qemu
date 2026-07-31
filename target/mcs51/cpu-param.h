fb7ee3a46d15caaeb5f5385c8e5e38c5117ac444
Chao Liu <chao.liu@processmission.com>
2026-07-31 22:57:47 +0800

    target/mcs51: Define the MCS-51 CPU family
    Add the shared CPU state, QOM declarations, address-space constants,
    and internal definitions used by classic MCS-51 and MCS-251 CPUs.
    
    Signed-off-by: Chao Liu <chao.liu@processmission.com>

---
+++ target/mcs51/cpu-param.h
@@ -0,0 +1,15 @@
+/*
+ * MCS-51 family target parameters
+ *
+ * Copyright (c) 2026 Process Mission
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef MCS51_CPU_PARAM_H
+#define MCS51_CPU_PARAM_H
+
+#define TARGET_PAGE_BITS 10
+#define TARGET_VIRT_ADDR_SPACE_BITS 24
+
+#endif
