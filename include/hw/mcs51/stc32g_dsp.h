86f5744ca160de4fb4bad51ca15b6e99a2bfe2cd
Chao Liu <chao.liu@processmission.com>
2026-07-31 22:58:39 +0800

    hw/mcs51: Add the STC32G DSP32 accelerator
    Implement the documented DSP32 command set, register interface, status
    flags, and migration state.
    
    Signed-off-by: Chao Liu <chao.liu@processmission.com>

---
+++ include/hw/mcs51/stc32g_dsp.h
@@ -0,0 +1,17 @@
+/*
+ * STC32G DSP32 command accelerator
+ *
+ * Copyright (c) 2026 Process Mission
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_MCS51_STC32G_DSP_H
+#define HW_MCS51_STC32G_DSP_H
+
+#include "qom/object.h"
+
+#define TYPE_STC32G_DSP "stc32g-dsp32"
+OBJECT_DECLARE_SIMPLE_TYPE(Stc32gDSPState, STC32G_DSP)
+
+#endif
